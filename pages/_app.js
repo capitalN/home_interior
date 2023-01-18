133ebb84dff3f76a70ce01220505d7c8fb5a249e
Shakil <shakilpp123456@gmail.com>
2023-01-18 22:12:52 +0530

    day-2_drawer_section

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import Cart from "@/components/Cart/cart";
 import { store } from "@/reducer/store";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
+import Cart from "@/components/Cart/cart";
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Heading>home</Heading>
-
     </>
   );
 }
