169f4157ed7b60ae6bda043319b30dfc6e3d9227
Nikhil Angolkar <angolkarnikhil@gmail.com>
2023-01-19 10:11:36 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,6 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
-      <ChakraProvider>
         <Navbar />
         <Component {...pageProps} />
       </ChakraProvider>
