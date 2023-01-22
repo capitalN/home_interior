82af5d8c672271cef5cabf4f8dbbdf3a2d7366c9
arun24hrs <2017arun.rana@gmail.com>
2023-01-22 16:15:22 +0530

    added responsivness

--- components/accountPage/sidebar.jsx
+++ components/accountPage/sidebar.jsx
@@ -2,7 +2,8 @@ import { Box, Divider, Heading, Stack, Text } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
+
 
 const Sidebar = ({linkEndPoint}) => {
    const route = useRouter();
@@ -15,26 +16,27 @@ const Sidebar = ({linkEndPoint}) => {
 
   return (
    <>
-   <Stack gap={4}>
+   
+   <Stack gap={8} display={{base: "none", md: "block"}}>
      <Box>
         <Heading size={"md"}>Hi {details.name}!</Heading>
-     </Box>
      <Divider colorScheme="black" />
+     </Box>
      <Box onClick = {()=> handlePageChange("/account")} _hover={{cursor: "pointer"}}>
         <Heading size={"md"} color={linkEndPoint=="/account" ? "brand.600" : "black"}>My Profile <ChevronRightIcon/></Heading>
         <Text fontSize="sm" color={"gray.500"}>Edit Your Account Details</Text>
-     </Box>
      <Divider colorScheme="black" />
+     </Box>
      <Box onClick = {()=> handlePageChange("/orders")} _hover={{cursor: "pointer"}}>
         <Heading size={"md"} color={linkEndPoint=="/orders" ? "brand.600" : "black"}>My Orders <ChevronRightIcon/></Heading>
         <Text fontSize="sm" color={"gray.500"}>Edit Your Account Details</Text>
-     </Box>
      <Divider colorScheme="black" />
+     </Box>
      <Box onClick = {()=> handlePageChange("/wallet")} _hover={{cursor: "pointer"}}>
         <Heading size={"md"} color={linkEndPoint=="/wallet" ? "brand.600" : "black"}>My Wallet <ChevronRightIcon/></Heading>
         <Text fontSize="sm" color={"brand.500"}>5000 Credits</Text>
-     </Box>
      <Divider colorScheme="black" />
+     </Box>
      <Box onClick = {()=> handlePageChange("/wishlist")} _hover={{cursor: "pointer"}}>
         <Heading size={"md"} color={linkEndPoint=="/wishlist" ? "brand.600" : "black"}>My Wishlist <ChevronRightIcon/></Heading>
         <Text fontSize="sm" color={"gray.500"}>Your Favourites, All In One Place</Text>
