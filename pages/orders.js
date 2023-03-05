b30f42ce84d5d59cfac8e515b79414bff7021dd9
Shakil <shakilpp123456@gmail.com>
2023-03-05 23:21:15 +0530

    added data into order page

--- pages/orders.js
+++ pages/orders.js
@@ -12,6 +12,9 @@ import {
   HStack,
   Link,
   VStack,
+  Grid,
+  Divider,
+  GridItem
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
@@ -19,11 +22,12 @@ import { useSelector } from "react-redux";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import { useEffect } from "react";
 import { useState } from "react";
+import Section1 from "@/components/CheckoutComponents/Section1";
 // import Link from "next/link";
 
 const Orders = () => {
   const loginStore = useSelector((store) => store.loginManager);
-  const { route } = useRouter();
+  const route = useRouter();
   const goBack = () => {
     route;
   };
@@ -35,26 +39,35 @@ const Orders = () => {
     setHistory(storage);
   }, []);
 
+  const RemoveData = (id)=>{
+    setHistory(()=>history?.filter((el)=>el.id!==id));
+    let data = history?.filter((el)=>el.id!==id)
+    localStorage.setItem("history",JSON.stringify(data))
+  }
+
   return !history.length ? (
     <Flex
-      width={"90%"}
-      py={10}
+      gap="0.5rem"
+      width={"75%"}
       m="auto"
+      p="0.5rem 1.5rem"
+      mb="7rem"
+      mt="4rem"
       direction={{ base: "column", md: "row" }}
     >
-      <Box onClick={goBack} display={{ base: "block", md: "none" }}>
-        <ArrowLeftIcon />
-        {" Go back"}
+      <Box flex="1.374" position="sticky" top="0rem" height="40%">
+        <Sidebar linkEndPoint={route} />
       </Box>
-      <Sidebar linkEndPoint={route} />
       <Flex
+        flex="4"
         width={{ base: "100%", md: "900px" }}
         alignItems="center"
+        // border="1px solid red"
         gap={4}
         direction={"column"}
-        paddingLeft={20}
       >
-        <Heading size={"lg"}>No Orders Here</Heading>
+      <Flex direction="column" justifyContent="center" alignItems="center">
+      <Heading size={"lg"}>No Orders Here</Heading>
         <Text color="gray.400" mb={10}>
           You Have Not Placed any orders yet
         </Text>
@@ -64,41 +77,65 @@ const Orders = () => {
           height={400}
           alt="Section Image"
         />
-        <Link href="/">
-          <Button bg="brand.800" marginTop={20} rounded={"none"}>
+        
+          <Button bg="brand.800" onClick={()=>route.push("/")} marginTop={20} rounded={"none"}>
             Continue Shopping
           </Button>
-        </Link>
+     
+      </Flex>
       </Flex>
     </Flex>
   ) : (
-    <Container maxW={"1200px"}>
+    <Flex   width={"75%"}
+    p="0.5rem 1.5rem"
+    m="auto"
+    mb="7rem"
+    mt="4rem"
+    >
+      <Flex>
+      <Box flex="1.4" position="sticky" top="0rem" height="40%">
+        <Sidebar linkEndPoint={route} />
+      </Box>
+      <Box flex="4">
       <br />
       <Heading textAlign="center">My Orders</Heading>
       <br />
-      {history.map((el) => (
-        <Flex
-          bgColor={"gray.100"}
-          flexWrap={"wrap"}
-          key={el.name}
-          align="top"
-          p="20px"
-          m="20px"
-          gap="20px"
-          as={Link}
-          to={`/furniture/products/productid/${el.id}`}
-        >
-          <Image src={el.image} alt={el.name} w="300px" />
-          <Stack justify={"space-between"} fontSize="25px" maxW="800px">
-            <Text fontWeight={"bold"}>{el.name}</Text>
-            <Text>CATEGORY: {el.category || "furniture"}</Text>
-            <Text>TYPE: {el.type}</Text>
-            <Text>PRICE: {el.price}</Text>
-            <Text>RATINGS: {el.rating || 3.5} </Text>
-          </Stack>
-        </Flex>
-      ))}
-    </Container>
+      {history?.map((item) => <Section1 key={item.id} {...item} />) && (
+              <Grid gap={"30px"} templateColumns="repeat(2, 1fr)">
+                {history.map((product) => (
+                  <GridItem key={product.id} borderLeft="5px solid orange" p={2}>
+                    <HStack gap={"5px"}>
+                      <Image src={product.image} w="35%" p={"5px"} />
+                      <Stack>
+                        <HStack justify={"space-between"}>
+                          <Text fontSize={"12px"} color="gray.500">{product.categories}</Text>
+                          <Link
+                              fontSize="xs"
+                              color="white"
+                              px={1}
+                              py={0.8}
+                              bg="red.300"
+                            onClick={() =>
+                             {
+                              RemoveData(product.id);
+                             }
+                            }
+                          >
+                            Remove
+                          </Link>
+                        </HStack>
+                        <Text fontSize="16px" fontWeight="bold" color="brand.600">{product.name}</Text>
+                        <Text fontWeight="bold">Price : ₹{product.price}</Text>
+                      </Stack>
+                    </HStack>
+                    <Divider />
+                  </GridItem>
+                ))}
+              </Grid>
+            )}
+      </Box>
+      </Flex>
+    </Flex>
   );
 };
 
